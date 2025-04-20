dee5555ed067011111998aff1f407fdaed72e2de
betaknight <betanight2002@gmail.com>
2025-04-20 13:15:29 -0700

    made a stat file

--- character_functions.py
+++ character_functions.py
@@ -13,3 +13,20 @@ class Character:
         self.spirit = 0
         self.instinct = 0
         self.presence = 0
+
+    def increase_stat(self, stat_name, amount):
+        if hasattr(self, stat_name):
+            setattr(self, stat_name, getattr(self, stat_name) + amount)
+
+    def decrease_stat(self, stat_name, amount):
+        if hasattr(self, stat_name):
+            setattr(self, stat_name, getattr(self, stat_name) - amount)
+
+    def set_stat(self, stat_name, new_value):
+        if hasattr(self, stat_name):
+            setattr(self, stat_name, new_value)
+
+    def get_stat(self, stat_name):
+        if hasattr(self, stat_name):
+            return getattr(self, stat_name)
+        return None
