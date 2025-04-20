a8dcd94a080d7a7a710313638edf115a67719198
betaknight <betanight2002@gmail.com>
2025-04-20 13:11:41 -0700

    created character

---
+++ character_functions.py
@@ -0,0 +1,15 @@
+class Character:
+    def __init__(self, name):
+        self.name = name
+
+        self.strength = 1
+        self.dexterity = 1
+        self.constitution = 1
+        self.intelligence = 1
+        self.wisdom = 1
+        self.charisma = 1
+
+        self.willpower = 0
+        self.spirit = 0
+        self.instinct = 0
+        self.presence = 0
