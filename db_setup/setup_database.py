476eb98bf62329ddae7c814b1d42e02f8900bc90
betaknight <betanight2002@gmail.com>
2025-04-21 19:12:35 -0700

    i

--- db_setup/setup_database.py
+++ db_setup/setup_database.py
@@ -2,7 +2,7 @@ import sqlite3
 
 def create_tables():
     conn = sqlite3.connect("infinity_game.db")
-    with open("system_database/init_stat_tree.sql", "r") as f:
+    with open("system_database/db_setup/init_stat_tree.sql", "r") as f:
         conn.executescript(f.read())
     conn.commit()
     conn.close()
