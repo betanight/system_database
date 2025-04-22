f9b354a581801eb205f34be5025177c90a5bcae3
betaknight <betanight2002@gmail.com>
2025-04-21 19:29:09 -0700

    k

--- db_setup/setup_database.py
+++ db_setup/setup_database.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-def create_tables():
+def create_tables(db_filename):
     conn = sqlite3.connect(db_filename)
     with open("system_database/db_setup/init_stat_tree.sql", "r") as f:
         conn.executescript(f.read())
@@ -8,8 +8,4 @@ def create_tables():
     conn.close()
 
 if __name__ == "__main__":
-    import sys
-    if len(sys.argv) < 2:
-        print("Usage: python3 setup_database.py [db_filename]")
-    else:
-        create_tables(sys.argv[1])
+    create_tables("infinity_game.db")
