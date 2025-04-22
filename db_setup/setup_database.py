5af13228b900ab2367f280f3ad341a323af93f61
betaknight <betanight2002@gmail.com>
2025-04-21 19:06:30 -0700

    database creation is now a function

--- db_setup/setup_database.py
+++ db_setup/setup_database.py
@@ -1,9 +1,11 @@
 import sqlite3
 
-conn = sqlite3.connect("infinity_game.db")
+def create_tables():
+    conn = sqlite3.connect("infinity_game.db")
+    with open("system_database/init_stat_tree.sql", "r") as f:
+        conn.executescript(f.read())
+    conn.commit()
+    conn.close()
 
-with open("system_database/init_stat_tree.sql", "r") as f:
-    conn.executescript(f.read())
-
-conn.commit()
-conn.close()
+if __name__ == "__main__":
+    create_tables()
