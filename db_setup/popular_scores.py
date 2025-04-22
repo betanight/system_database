5746bec854d3ca1deb6d1de14e427dfd39171a53
betaknight <betanight2002@gmail.com>
2025-04-21 19:04:09 -0700

    score functions

--- db_setup/popular_scores.py
+++ db_setup/popular_scores.py
@@ -1,26 +1,37 @@
 import sqlite3
 
-conn = sqlite3.connect("infinity_game.db")
-cursor = conn.cursor()
+def insert_primary_scores():
+    conn = sqlite3.connect("infinity_game.db")
+    cursor = conn.cursor()
 
-primary_stats = [
-    'Strength',
-    'Dexterity',
-    'Constitution',
-    'Intelligence',
-    'Wisdom',
-    'Charisma'
-]
+    primary_stats = [
+        'Strength',
+        'Dexterity',
+        'Constitution',
+        'Intelligence',
+        'Wisdom',
+        'Charisma'
+    ]
 
-secondary_stats = [
-    'Willpower',
-    'Spirit',
-    'Instinct',
-    'Presence'
-]
+    cursor.executemany("INSERT OR IGNORE INTO primary_scores (name) VALUES (?)", [(stat,) for stat in primary_stats])
+    conn.commit()
+    conn.close()
 
-cursor.executemany("INSERT OR IGNORE INTO primary_scores (name) VALUES (?)", [(stat,) for stat in primary_stats])
-cursor.executemany("INSERT OR IGNORE INTO secondary_scores (name) VALUES (?)", [(stat,) for stat in secondary_stats])
+def insert_secondary_scores():
+    conn = sqlite3.connect("infinity_game.db")
+    cursor = conn.cursor()
 
-conn.commit()
-conn.close()
+    secondary_stats = [
+        'Willpower',
+        'Spirit',
+        'Instinct',
+        'Presence'
+    ]
+
+    cursor.executemany("INSERT OR IGNORE INTO secondary_scores (name) VALUES (?)", [(stat,) for stat in secondary_stats])
+    conn.commit()
+    conn.close()
+
+if __name__ == "__main__":
+    insert_primary_scores()
+    insert_secondary_scores()
